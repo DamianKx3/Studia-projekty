8b29b74a16d1093104dd8c7394a7f5a8549359a7
GitPasteX <damiankrokosinski@gmail.com>
2026-06-26 16:39:16 +0200

    Update Form1.Designer.cs

--- DeksTop_/Form1.Designer.cs
+++ DeksTop_/Form1.Designer.cs
@@ -64,7 +64,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(359, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Projek zaliczeniowy na grafikę komputerową    Damian Krokosiński 255ICA";
+            this.label1.Text = "Projek zaliczeniowy na grafikę komputerową    Damian K##########";
             // 
             // groupBox1
             // 
